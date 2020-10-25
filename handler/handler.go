912d005c56f63d5fc941f222f4325cc792a50c46
ahhzaky <siahmadzaky@gmail.com>
2020-10-26 00:25:01 +0700

    :gear: in process

--- handler/handler.go
+++ handler/handler.go
@@ -2,29 +2,42 @@ package handler
 
 import (
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
+	"path"
 	"strconv"
 )
 
-func HomeHandler(w http.ResponseWriter, r *http.Request)  {
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
 	log.Println(r.URL.Path)
 	if r.URL.Path != "/" {
 		http.NotFound(w, r)
 		return
 	}
-	w.Write([]byte("welcome to home"))
+	tmpl, err := template.ParseFiles(path.Join("views", "index.html"))
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "Error is happening, keep calm", http.StatusInternalServerError)
+		return
+	}
+	err = tmpl.Execute(w, nil)
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "Error is happening, keep calm", http.StatusInternalServerError)
+		return
+	}
 }
 
-func HelloHandler(w http.ResponseWriter, r *http.Request)  {
+func HelloHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("hello world, saya sedang belajar"))
 }
 
-func MarioHandler(w http.ResponseWriter, r *http.Request)  {
+func MarioHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("hello mario, sedang apa?"))
 }
 
-func ProductHandler(w http.ResponseWriter, r *http.Request)  {
+func ProductHandler(w http.ResponseWriter, r *http.Request) {
 	id := r.URL.Query().Get("id")
 
 	idNumb, err := strconv.Atoi(id)
